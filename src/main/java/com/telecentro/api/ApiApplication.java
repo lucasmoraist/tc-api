dcbabc2e37606ad057d85080d76404f853065846
lucasmoraist <luksmnt1101@gmail.com>
2025-01-08 08:56:23 -0300

    feat: finishing configs

--- src/main/java/com/telecentro/api/ApiApplication.java
+++ src/main/java/com/telecentro/api/ApiApplication.java
@@ -1,10 +1,5 @@
 package com.telecentro.api;
 
-import com.telecentro.api.domain.dto.agent.AgentRequest;
-import com.telecentro.api.service.AgentService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
