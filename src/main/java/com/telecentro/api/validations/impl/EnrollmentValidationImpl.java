71e1f6c1ea3e4e5fac08fec892af3de15a83316f
lucasmoraist <luksmnt1101@gmail.com>
2025-01-08 13:53:26 -0300

    feat: enhance enrollment validation to close full courses and save state

--- src/main/java/com/telecentro/api/validations/impl/EnrollmentValidationImpl.java
+++ src/main/java/com/telecentro/api/validations/impl/EnrollmentValidationImpl.java
@@ -1,15 +1,28 @@
 package com.telecentro.api.validations.impl;
 
 import com.telecentro.api.domain.entities.Course;
+import com.telecentro.api.domain.entities.Student;
+import com.telecentro.api.repository.CourseRepository;
 import com.telecentro.api.validations.EnrollmentValidation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EnrollmentValidationImpl implements EnrollmentValidation {
+
+    @Autowired
+    private CourseRepository courseRepository;
+
     @Override
     public void validate(Course course) {
-        if (course.getStudents().size() == 16) {
-            throw new IllegalStateException("Course is full");
+        long studentsConfirmed = course.getStudents().stream()
+                .filter(Student::isConfirmed)
+                .count();
+
+        if (studentsConfirmed >= 16) {
+            course.setOpen(false);
+            this.courseRepository.save(course);
+            throw new RuntimeException("Course is full");
         }
     }
 }
