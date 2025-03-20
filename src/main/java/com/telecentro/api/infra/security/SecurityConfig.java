6b0091aff06d39525c0140266de84d1e1547861d
Lucas de Morais <112484844+lucasmoraist@users.noreply.github.com>
2025-03-20 17:53:13 -0300

    Update SecurityConfig.java

--- src/main/java/com/telecentro/api/infra/security/SecurityConfig.java
+++ src/main/java/com/telecentro/api/infra/security/SecurityConfig.java
@@ -56,7 +56,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedOrigins(List.of("https://tc-api-production.up.railway.app/"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("Authorization"));
