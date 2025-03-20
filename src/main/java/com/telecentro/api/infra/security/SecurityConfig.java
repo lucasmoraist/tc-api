bee583bf45208fbaf8d85006c242e7b38962a832
Lucas de Morais <112484844+lucasmoraist@users.noreply.github.com>
2025-03-20 17:48:54 -0300

    Update SecurityConfig.java

--- src/main/java/com/telecentro/api/infra/security/SecurityConfig.java
+++ src/main/java/com/telecentro/api/infra/security/SecurityConfig.java
@@ -56,7 +56,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173/", "https://tcsobei.org/", "https://cozy-respect-production.up.railway.app/"));
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("Authorization"));
