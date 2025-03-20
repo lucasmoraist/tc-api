b90262e2aa239fa61db299e2964f1eeb91accb39
Lucas de Morais <112484844+lucasmoraist@users.noreply.github.com>
2025-03-20 17:48:31 -0300

    Update CorsConfig.java

--- src/main/java/com/telecentro/api/infra/web/CorsConfig.java
+++ src/main/java/com/telecentro/api/infra/web/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173/", "https://tcsobei.org/", "https://cozy-respect-production.up.railway.app/")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }};
